355b12029b54dde0e6ce4c33ae0ff98283be304b
f_nosrati <f_nosrati@SHL-62>
2023-05-09 21:31:22 +0330

    Add Project And Infra Codes

---
+++ CommandInterceptionWebApplication/CommandInterceptionWebApplication/Repositorys/ProductWriteRepository.cs
@@ -0,0 +1,24 @@
+﻿using CommandInterceptionWebApplication.Domain;
+using CommandInterceptionWebApplication.Domain.Entitys;
+
+namespace CommandInterceptionWebApplication.Repositorys
+{
+    public class ProductWriteRepository : IProductWriteRepository
+    {
+        public async Task<int> CreateProductAsync(Product product)
+        {
+            await Task.Delay(1000);
+            return Random.Shared.Next();
+        }
+
+        public async Task DeleteProductAsync(int productId)
+        {
+            await Task.Delay(1000);
+        }
+
+        public async Task UpdateProductAsync(Product product)
+        {
+            await Task.Delay(1000);
+        }
+    }
+}
---
+++ CommandInterceptionWebApplication/CommandInterceptionWebApplication/Services/Contracts/IProductServices.cs
@@ -0,0 +1,18 @@
+﻿using CommandInterceptionWebApplication.InputModels.ProductInputModels;
+using CommandInterceptionWebApplication.ViewModels.ProductViewModels;
+
+namespace CommandInterceptionWebApplication.Services.Contracts
+{
+    public interface IProductServices
+    {
+        Task<int> CreateProductAsync(CreateProductInputModel inputModel);
+
+        Task UpdateProductAsync(UpdateProductInputModel inputModel);
+
+        Task DeleteProductAsync(int productId);
+
+        ValueTask<ProductViewModel> GetProductAsync(int productId);
+
+        ValueTask<IEnumerable<ProductViewModel>> GetProductsAsync();
+    }
+}
